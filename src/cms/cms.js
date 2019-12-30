b004cb45bab984bfe54e2a92c8c7cc2c4ca249b0
Trent Going <trent.going@gmail.com>
2019-12-30 15:45:18 -0500

    removed extraneous pages

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,14 +1,10 @@
 import CMS from 'netlify-cms-app'
-// import uploadcare from 'netlify-cms-media-library-uploadcare'
-// import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 import IndexPagePreview from './preview-templates/IndexPagePreview';
 import ResumePagePreview from './preview-templates/ResumePagePreview';
 import GalleryPagePreview from './preview-templates/GalleryPagePreview';
 import MediaPagePreview from './preview-templates/MediaPagePreview';
 
-// CMS.registerMediaLibrary(uploadcare)
-// CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview);
 CMS.registerPreviewTemplate('resume', ResumePagePreview);
